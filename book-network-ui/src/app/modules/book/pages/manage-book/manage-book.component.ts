801e583afca006791f3a388b80abc65c924f1877
ROUSSI Hatim <115400374+ROVSSI@users.noreply.github.com>
2024-08-03 12:17:19 +0100

    [feat] add a book

--- book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
+++ book-network-ui/src/app/modules/book/pages/manage-book/manage-book.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {BookRequest} from "../../../../services/models/book-request";
+import {BookService} from "../../../../services/services/book.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-manage-book',
@@ -7,4 +10,51 @@ import { Component } from '@angular/core';
 })
 export class ManageBookComponent {
 
+  bookRequest: BookRequest = {authorName: "", isbn: "", synopsis: "", title: ""};
+  errorMsg: Array<string> = [];
+  selectedBookCover: any;
+  selectedPicture: string | undefined;
+
+  constructor(
+    private bookService: BookService,
+    private route: Router
+  ) {
+  }
+
+  onFileSelected(event: any) {
+    this.selectedBookCover = event.target.files[0];
+    console.log(this.selectedBookCover);
+
+    if (this.selectedBookCover) {
+
+      const reader = new FileReader();
+      reader.onload = () => {
+        this.selectedPicture = reader.result as string;
+      };
+      reader.readAsDataURL(this.selectedBookCover);
+    }
+  }
+
+  saveBook() {
+    this.bookService.saveBook({
+      body: this.bookRequest
+    }).subscribe({
+      next: (bookId) => {
+        this.bookService.uploadBookCoverPicture({
+          'book-id': bookId,
+          body: {
+            file: this.selectedBookCover
+          }
+        }).subscribe({
+          next: () => {
+            this.route.navigate(['/books/my-books']);
+          }
+        });
+      },
+      error: (err) => {
+        console.log(err.error);
+        this.errorMsg = err.error.validationErrors;
+      }
+    });
+  }
 }
